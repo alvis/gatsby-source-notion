c48f8ad4ef474318036987a9780eb690e06e0f17
Alvis HT Tang <alvis@hilbert.space>
2021-06-10 11:14:50 +0000

    feat(type): provided data types in relation to users

--- source/types/user.ts
+++ source/types/user.ts
@@ -5,7 +5,7 @@
  * See the LICENSE file for details.
  * -------------------------------------------------------------------------
  *
- * @summary   Collection of types related to Notion API
+ * @summary   Collection of user related types
  *
  * @author    Alvis HT Tang <alvis@hilbert.space>
  * @license   MIT
@@ -13,6 +13,20 @@
  * -------------------------------------------------------------------------
  */
 
-/* istanbul ignore file */
+interface UserBase<T extends string> {
+  object: 'user';
+  id: string;
+  type: T;
+  name: string;
+  avatar_url?: string;
+}
 
-export * from './user';
+export interface PersonUser extends UserBase<'person'> {
+  person?: {
+    email: string;
+  };
+}
+
+export type BotUser = UserBase<'bot'>;
+
+export type User = PersonUser | BotUser;
