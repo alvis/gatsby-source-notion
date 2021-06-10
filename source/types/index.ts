66e33b142071cdc96414208c9ef96e0016750c3f
Alvis HT Tang <alvis@hilbert.space>
2021-06-10 11:12:50 +0000

    feat(type): add a placeholder for types

---
+++ source/types/index.ts
@@ -0,0 +1,16 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Collection of types related to Notion API
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2021 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+/* istanbul ignore file */
